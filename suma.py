8346abe1988214515e81937617afae97ba92ff24
santroblex18 <roblessant2020@gmail.com>
2024-05-13 19:36:56 -0500

    agregar archivo suma y rama suma

---
+++ suma.py
@@ -0,0 +1,5 @@
+valor1 = int(input("Escriba un primer número: "))
+valor2 = int(input("Escriba un segundo número: "))
+resultado = valor1 + valor2
+
+suma = print(f'{valor1} + {valor2} = {resultado}')
